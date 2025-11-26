bc673f80a1cc8212e4422f69580e2d30ee82a9c3
Abhay Verma <lugochan6674@gmail.com>
2025-11-27 01:15:55 +0530

    3.c

--- lab file/experiment 7/3.c
+++ lab file/experiment 7/3.c
@@ -1,5 +1,6 @@
 //Create a Book structure containing book_id, title, author name and price. Write a C program to pass a structure as a function argument and print the book details. 
 #include<stdio.h>
+
 struct book
 {
     int id;
@@ -7,34 +8,36 @@ struct book
     char author[50];
     int price;
 };
-
+void display(struct book b)
+{
+    printf("BOOK ID - %d   BOOK TITLE - %s   BOOK AUTHOR - %s   BOOK PRICE - %d\n",
+           b.id, b.title, b.author, b.price);
+}
 int main()
 {
     int n;
     printf("how many book you want to print = ");
     scanf("%d", &n);
     struct book b[n];
+
     for(int i = 0; i < n ; i++)
     {
         printf("__BOOK NO.%d__\n", i+1);
-
         printf(" enter the book id = ");
         scanf("%d", &b[i].id);
-
         printf(" enter the book title = ");
         scanf("%s", b[i].title);
-
         printf(" enter the book author name = ");
         scanf("%s", b[i].author);
-
         printf(" enter the book price = ");
         scanf("%d", &b[i].price);
     }
     printf(" ________________________BOOK DETAILS_______________________ \n");
     for(int i = 0; i < n ; i++)
     {
-        printf("BOOK ID - %d   BOOK TITLE - %s   BOOK AUTHOR - %s   BOOK PRICE - %d\n",
-               b[i].id, b[i].title, b[i].author, b[i].price);
+        display(b[i]);  // passing each structure to function
     }
     return 0;
 }
+
+
